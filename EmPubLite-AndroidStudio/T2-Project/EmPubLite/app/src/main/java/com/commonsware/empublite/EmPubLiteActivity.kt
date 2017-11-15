c888971a15c45900a1f780a3d561273bcf663d8b
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2017-11-15 21:34:39 +0100

    add kotlin support, gitignore

---
+++ EmPubLite-AndroidStudio/T2-Project/EmPubLite/app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.kt
@@ -0,0 +1,12 @@
+package com.commonsware.empublite
+
+import android.app.Activity
+import android.os.Bundle
+
+class EmPubLiteActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+}
